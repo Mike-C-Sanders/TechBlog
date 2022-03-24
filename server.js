900c0b2491a1aae69eb7688dc7b70330472fb982
Mike Sanders <msanders331@gmail.com>
2022-03-23 23:01:14 -0700

    added server file base code to include packages and server base

---
+++ server.js
@@ -0,0 +1,41 @@
+//Required packages
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+//sequelize connection
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+//default port 3001 or use a delpoyable port such as heroku 
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({helpers});
+
+const sess ={
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(use));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() =>{
+    app.listen(PORT, () => console.log('Now listening'));
+})
