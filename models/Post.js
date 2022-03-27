8c24285fae6edee4217dcf3ff853e1302777dd5e
Mike Sanders <msanders331@gmail.com>
2022-03-26 21:10:34 -0700

    added the post model

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {};
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id:{
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'user',
+                key: 'id',
+            }
+        }
+
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
--- models/User.js
+++ models/User.js
@@ -54,3 +54,5 @@ User.init(
         modelName: 'user',
     }
 )
+
+module.exports = User;
