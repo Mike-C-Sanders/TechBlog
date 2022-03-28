49ccaf34aedae35d9bcec9e11b9eddacf5372469
Mike Sanders <msanders331@gmail.com>
2022-03-27 20:51:55 -0700

    added the delete route to the blogRoutes file.

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -17,6 +17,24 @@ router.post('/', withAuth, async (req, res) =>{
         res.status(400).json(err);
 
     }
-})
+});
+
+//delete a blog post. Authentication required prior to deleting
+router.delete('/:id', withAuth, async (req, res) =>{
+    try{
+        const blogPostData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            }
+        });
+        if(!blogPostData){
+            res.status(404).json({message: `No blog post found with this id!`});
+        }
+        res.status(200).json(blogPostData);
+    }catch(err){
+        res.status(400).json(err);
+    }
+});
 
 module.exports = router;
