2c9c6aca108cef7a01e024c74ef04a62e91367d7
Mike Sanders <msanders331@gmail.com>
2022-03-29 21:10:00 -0700

    updated index routes folder

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const homeRoutes = require('./homeRoutes');
+const apiRoutes = require('./api/index');
+const dashboardRoutes = require('./dashboardRoutes');
+
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+
+module.exports = router;
